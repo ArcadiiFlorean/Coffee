704b0289a555e941b8e3952d6e0b022ed8d73bee
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-02-06 15:30:58 +0000

    Adăugat fișiere

---
+++ js.js
@@ -0,0 +1,18 @@
+//Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
