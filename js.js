506c4e7836dc2071549b02f80e33286976de4417
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-02-06 20:38:15 +0000

    my images folder

--- js.js
+++ js.js
@@ -1,18 +1,29 @@
 //Initialize Swiper
 
-const swiper = new Swiper('.slider-wrapper', {
-
-
+const swiper = new Swiper(".slider-wrapper", {
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
-
 });
+
+//nav_button
+// Selectează butonul și meniul
+const menuButton = document.querySelector('.menu_open_button');
+const navMenu = document.querySelector('.nav_menu');
+
+// Verifică dacă elementele au fost selectate corect
+if (menuButton && navMenu) {
+  // Adaugă un eveniment de clic pe butonul de meniu
+  menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('show');  // Adaugă sau îndepărtează clasa 'show'
+  });
+} else {
+  console.error('Elementele nu au fost găsite.');
+}
