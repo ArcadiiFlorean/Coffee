45ec1b65ac17d4c1c6b83b0a62ae6c642bfcdd0e
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-02-07 20:42:58 +0000

    Big Work

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ const swiper = new Swiper(".slider-wrapper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
+});9
 
 //nav_button
 // Selectează butonul și meniul
@@ -26,4 +26,13 @@ if (menuButton && navMenu) {
   });
 } else {
   console.error('Elementele nu au fost găsite.');
+}
+
+
+//hover for menu_open_button
+function toggleButtonEffect() {
+  const button = document.querySelector('.menu_open_button');
+  
+  // Adaugă sau elimină clasa care controlează efectul de hover
+  button.classList.toggle('active');
 }
