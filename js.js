6505a95fb3c6982447590c19e6cd8592758d42c5
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-02-09 15:27:41 +0000

    add images

--- js.js
+++ js.js
@@ -11,28 +11,28 @@ const swiper = new Swiper(".slider-wrapper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});9
+});
+9;
 
 //nav_button
 // Selectează butonul și meniul
-const menuButton = document.querySelector('.menu_open_button');
-const navMenu = document.querySelector('.nav_menu');
+const menuButton = document.querySelector(".menu_open_button");
+const navMenu = document.querySelector(".nav_menu");
 
 // Verifică dacă elementele au fost selectate corect
 if (menuButton && navMenu) {
   // Adaugă un eveniment de clic pe butonul de meniu
-  menuButton.addEventListener('click', () => {
-    navMenu.classList.toggle('show');  // Adaugă sau îndepărtează clasa 'show'
+  menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("show"); // Adaugă sau îndepărtează clasa 'show'
   });
 } else {
-  console.error('Elementele nu au fost găsite.');
+  console.error("Elementele nu au fost găsite.");
 }
-
 
 //hover for menu_open_button
 function toggleButtonEffect() {
-  const button = document.querySelector('.menu_open_button');
-  
+  const button = document.querySelector(".menu_open_button");
+
   // Adaugă sau elimină clasa care controlează efectul de hover
-  button.classList.toggle('active');
+  button.classList.toggle("active");
 }
